30b1cd141c173faf56258da262e5b2cade109d73
tvc12 <meomeocf98@gmail.com>
2019-10-11 23:47:24 +0700

    Setup vnexpress crawler

---
+++ bin/domain/news.dart
@@ -0,0 +1,19 @@
+class News {
+  String id;
+  String lang;
+  String source;
+  String originId;
+  List<String> categories;
+  String headline;
+  String description;
+  List<String> contents;
+  String htmlContent;
+  String url;
+  int status;
+  String author;
+  List<String> authors;
+  String thumbnail;
+  int updatedTime;
+  int publishedTime;
+  int createdTime;
+}
